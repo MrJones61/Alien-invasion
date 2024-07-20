7e8ee3f24e574830e172ae303c75280793eaa62a
MrJones61 <mrjones61dylan@gmail.com>
2024-07-20 14:38:46 +0545

    Scoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ from time import sleep
 import pygame
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -19,8 +20,9 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("ALIEN INVASION BY MR.JONES")
 
-        # Create an instance of the game statistics
+        # Create an instance to store game statistics, and create a scoreboard
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -91,6 +93,8 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+        # Draw the score information.
+        self.sb.show_score()
 
         # Draw the play button if the gmae is inactive
         if not self.stats.game_active:
@@ -171,11 +175,16 @@ class AlienInvasion:
         # Check for any bullets that have hit aliens.
         # If so get rid of the bullet and the alien.    
         collisions = pygame.sprite.groupcollide(self.bullets,self.aliens,True,True)
+        
+        if collisions:
+            self.stats.score += self.settings.alien_points
+            self.sb.prep_score( )
 
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _ship_hit(self):
         """ Respond to the ship being hit by an alien."""
--- settings.py
+++ settings.py
@@ -1,13 +1,12 @@
 class Settings:
     """A class to store all the settings for alien invasion"""
     def __init__(self):
-        """Initialize the game setting."""
+        """Initialize the game's static settings."""
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (50,150,215)
 
         # Ship settings
-        self.ship_speed = 1.5
         self.ship_limit = 2
 
         # Bullet settings
@@ -16,9 +15,37 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (230, 0, 0)
         self.bullets_allowed = 10
-        # Alien setting
         
-        self.alien_speed = 1.0
+        # Alien setting
         self.fleet_drop_speed = 20
-        # -1 and  1 represent left and right fleet direction respectively.
-        self.fleet_direction = -1
+        
+        # How quickly the game speed's up.
+        self.speedup_scale = 1.1
+        
+        self.initialize_dynamic_settings()
+        
+    def initialize_dynamic_settings(self):
+        """ Initialize the setting that change throught the game."""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3
+        self.alien_speed = 1
+        
+        # fleet drn of 1 represents right and -1 represents left.
+        self.fleet_direction = 1
+        #Scoring
+        self.alien_points = 50
+            
+            
+    def increase_speed(self):
+        """ Increase speed settings."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+        
+        
+        
+            
+            
+            
+    
+            
